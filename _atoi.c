1209851be3f2628d46bfcb3ecd82d24ff2291a9f
root <amr.magdy.hassan95@gmail.com>
2023-04-28 14:22:23 -0700

    added a few more details

--- _atoi.c
+++ _atoi.c
@@ -13,36 +13,22 @@ int interactive(info_t *info)
 
 /**
  * is_delim - checks  delimeter
- * @c: char
+ * @ch: char
  * @delim: the delimeter 
  * Return: 1 if true, 0 if false
  */
-int is_delim(char c, char *delim)
+int is_delim(char ch, char *delim)
 {
 	while (*delim)
-		if (*delim++ == c)
+		if (*delim++ == ch)
 			return (1);
 	return (0);
 }
 
 /**
- * _isalpha - checks for alphabetic character
- * @c: The character 
- * Return: 1 if c is alphabetic, 0 otherwise
- */
-
-int _isalpha(int c)
-{
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-		return (1);
-	else
-		return (0);
-}
-
-/**
  * _atoi - converts a string to an integer
  * @s: the string to be converted
- * Return: 0 if no numbers in string, converted number otherwise
+ * Return: 0 if no numbers in string
  */
 
 int _atoi(char *s)
@@ -73,3 +59,16 @@ int _atoi(char *s)
 	return (output);
 }
 
+/**
+ * _isalpha - checks for alphabetic character
+ * @ch: The character
+ * Return: 1 if c is alphabetic, 0 otherwise
+ */
+
+int _isalpha(int ch)
+{
+        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+                return (1);
+        else
+                return (0);
+}
